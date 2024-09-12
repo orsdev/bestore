b446d14b255266b748993d5d6f8fda06bbe2e17d
Ogundare Samuel <30951791+orsdev@users.noreply.github.com>
2024-09-12 06:00:21 +0100

    refactor: change files to lower-case

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,4 @@
 export * from "./product.interface";
 export * from "./product.interface";
 export * from "./common.interface";
-export * from "./IRequestConfig.interface";
-export * from "./IResponse.interface";
-export * from "./ICart.interface";
+export * from "./cart.interface";
